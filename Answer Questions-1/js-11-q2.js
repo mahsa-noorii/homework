64907a0d2ed9fb319fcdc6bb8a8c665f28e9fea5
mahsa-noorii <willow231181@gmail.com>
2023-12-19 15:03:49 +0330

    deleted

--- Answer Questions-1/js-11-q2.js
+++
@@ -1,7 +0,0 @@
-// ? Question-2:completed this table(Y or N )
-
-// | Keyword      | Global Scope | Block Scope | Function Scope | Can be re-assign | Can be re-declare |
-// |--------------|--------------|-------------|----------------|------------------|-------------------|
-// | var          |      Y        |     y        |        N        |        Y          |        Y           |
-// | let          |      Y        |     N        |        N        |        Y          |       N            |
-// | const        |      Y        |     N        |        N        |        N         |        N           |
