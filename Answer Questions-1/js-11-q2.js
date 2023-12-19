472e66603adc848c5331f691eb77581e18d78050
mahsa-noorii <willow231181@gmail.com>
2023-12-19 14:54:40 +0330

    homework1

---
+++ Answer Questions-1/js-11-q2.js
@@ -0,0 +1,7 @@
+// ? Question-2:completed this table(Y or N )
+
+// | Keyword      | Global Scope | Block Scope | Function Scope | Can be re-assign | Can be re-declare |
+// |--------------|--------------|-------------|----------------|------------------|-------------------|
+// | var          |      Y        |     y        |        N        |        Y          |        Y           |
+// | let          |      Y        |     N        |        N        |        Y          |       N            |
+// | const        |      Y        |     N        |        N        |        N         |        N           |
---
+++ Answer Questions-1/js-11-q5.js
@@ -0,0 +1,8 @@
+// What is the result of the code below?
+const hello = "what's up aps?!";
+const obj = {
+  func() {
+    return hello;
+  },
+};
+console.log(obj.func()); // answer is?: what's up aps?!
