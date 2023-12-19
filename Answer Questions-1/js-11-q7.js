33ce8be877cffa929b5060e1b7e5584c493f0c5e
Mahsa Noori <142102273+mahsa-noorii@users.noreply.github.com>
2023-12-19 15:01:49 +0330

    Delete Answer Questions-1 directory

--- Answer Questions-1/js-11-q7.js
+++
@@ -1,7 +0,0 @@
-// Complete the code below to display "{ method: [Function: method] }" in the console when the method function is executed.
-const myObject = {
-  method() {
-    console.log(this)
-  },
-};
-myObject.method();
